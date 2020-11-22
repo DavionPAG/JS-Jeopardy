9b83f08b1adc760e963fa6cbdd2eb05a5eef1a03
Terrence Jamal Simms <Tjs@omegacc.com>
2020-11-21 17:18:20 -0800

    Worked on Endgame logic

--- js/end.js
+++ js/end.js
@@ -3,14 +3,34 @@
 // var firstPlace = document.createElement('p'); NOT NECCESSARY
 // firstPlace.setAttribute('id', 'Winner'); NOT NECCESARY
 
+var playersArray = [];
 
+function Players(name) {
+  this.name = name;
+  this.score = 0;
+  playersArray.push(this);
+}
+
+new Players ('a');
+new Players ('b');
+new Players ('c');
+new Players ('d');
+
+// GLOBAL VARIABLES
+// var playersArray = [];
+
+// Placements Variable for the sake of showing Scores
+
+// Variable for linking Elements to page
+var results = document.getElementById('results');
 // Function used to create each Players position on the score board!
 function playerGenerator() {
   // Some sort of Filter for highest number
-  for (var i = 0; i <= playerArray.length; i++) {
+  for (var i = 0; i < playersArray.length; i++) {
+    var playerid = `Player${i}`;
     var placements = document.createElement('p');
-    placements.setAttribute('id', `Player${i}`); // Remember it is 0-3 for the array CSS Purposes
+    placements.setAttribute('id', playerid); // Remember it is 0-3 for the array CSS Purposes
     placements.textContent = `${playersArray[i].name} your score was ${playersArray[i].score}!`;
+    results.appendChild(placements);
   }
 }
-
