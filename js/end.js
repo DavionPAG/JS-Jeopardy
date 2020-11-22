377683436d8839f3163011ab788efedd34a0e98e
root <root@Vektur.localdomain>
2020-11-21 16:32:45 -0800

    Local Storage added

--- js/end.js
+++ js/end.js
@@ -3,6 +3,10 @@
 // var firstPlace = document.createElement('p'); NOT NECCESSARY
 // firstPlace.setAttribute('id', 'Winner'); NOT NECCESARY
 
+// Get player data from local storage
+
+var playerArray = localStorage.getItem('players');
+playerArray = JSON.parse(playerArray);
 
 // Function used to create each Players position on the score board!
 function playerGenerator() {
@@ -10,7 +14,9 @@ function playerGenerator() {
   for (var i = 0; i <= playerArray.length; i++) {
     var placements = document.createElement('p');
     placements.setAttribute('id', `Player${i}`); // Remember it is 0-3 for the array CSS Purposes
-    placements.textContent = `${playersArray[i].name} your score was ${playersArray[i].score}!`;
+    placements.textContent = `${playerArray[i].name} your score was ${playerArray[i].score}!`;
   }
 }
+
+// playerGenerator();
 
