9a1bd513fc3773d4e7c2cac848c668bc17d34fed
Terrence Jamal Simms <Tjs@omegacc.com>
2020-11-23 18:38:25 -0800

    Worked on the game section

--- js/end.js
+++ js/end.js
@@ -8,20 +8,9 @@ var playersArray = [];
 
 // Get player data from local storage
 
-var playerArray = localStorage.getItem('players');
-playerArray = JSON.parse(playerArray);
+// var playerArray = localStorage.getItem('players');
+// playerArray = JSON.parse(playerArray);
 
-
-function Players(name) {
-  this.name = name;
-  this.score = 0;
-  playersArray.push(this);
-}
-
-new Players ('a');
-new Players ('b');
-new Players ('c');
-new Players ('d');
 
 // GLOBAL VARIABLES
 // var playersArray = [];
@@ -36,7 +25,7 @@ function playerGenerator() {
   for (var i = 0; i < playersArray.length; i++) {
     var playerid = `Player${i}`;
     var placements = document.createElement('p');
-    
+
     placements.setAttribute('id', playerid); // Remember it is 0-3 for the array CSS Purposes
     placements.textContent = `${playersArray[i].name} your score was ${playersArray[i].score}!`;
     results.appendChild(placements);
